dd3aa4ee8295ce6671028c25da4f2bae928d888c
JulesGems <34382305+JulesGems@users.noreply.github.com>
2020-11-18 09:25:24 -0500

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch-DESKTOP-CNL26LT.js
@@ -0,0 +1,18 @@
+var fixedRect;
+var movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+  
+  movingRect = createSprite(400, 200, 80, 30);
+  movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background("black");  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  drawSprites();
+}
